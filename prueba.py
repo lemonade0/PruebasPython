634ab1e2ebc26c0d3b3180f91149983d6ec03b7d
alumno-mañana <tengounpuntito@gmail.com>
2019-06-26 09:42:45 +0200

    Ejercicios de prueba,

--- prueba.py
+++ prueba.py
@@ -14,6 +14,7 @@ Ejercicio 4
 Escribir un programa que le diga al usuario que ingrese una cadena. El programa tiene que evaluar la cadena y decir
 cuantas letras mayúsculas tiene. 
 """
+"""
 import re
 def contar_mayusculas(texto):
     textoSoloMayusculas = re.sub('[^A-Z]', '', texto)
@@ -21,3 +22,9 @@ def contar_mayusculas(texto):
 
 texto = input("Texto Con Mayusculas y minusculas y num")
 contar_mayusculas(texto)
+"""
+
+laSuma = 0
+    for i in listaNumeros:
+        laSuma = laSuma + i
+    return laSuma
