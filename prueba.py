acaa130d6d3ebac49038383a161de65c55b29210
alumno-mañana <tengounpuntito@gmail.com>
2019-06-26 10:10:12 +0200

    Ejercicios de prueba, mas otros ejercicios

--- prueba.py
+++ prueba.py
@@ -24,8 +24,18 @@ texto = input("Texto Con Mayusculas y minusculas y num")
 contar_mayusculas(texto)
 """
 """
-laSuma = 0
+Ejercicio 1
+Escriba una función que tome una lista de números y devuelva la suma acumulada, es decir, una nueva lista donde el
+primer elemento es el mismo, el segundo elemento es la suma del primero con el segundo, el tercer elemento es la suma
+del resultado anterior con el siguiente elemento y así sucesivamente. 
+Por ejemplo, la suma acumulada de [1,2,3] es [1, 3, 6].
+
+"""
+def sumalista(listaNumeros):
+    laSuma = 0
     for i in listaNumeros:
         laSuma = laSuma + i
-    return laSuma
-"""
+        lista += [laSuma]
+        return laSuma
+
+print(sumalista([1,2,3]))
